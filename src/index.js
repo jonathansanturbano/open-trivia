0ee05d9a8119f12e310f6f61474d1b15c23d541a
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2022-05-19 10:09:38 +1000

    Trivia Day

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Hello from src/index.js!");
+import { Application } from "@hotwired/stimulus";
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
+
+window.Stimulus = Application.start();
+const context = require.context("./controllers", true, /\.js$/);
+/* eslint-disable */
+Stimulus.load(definitionsFromContext(context));
